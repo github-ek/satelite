209f6a3422020bdf6e2c3f0a351da7707c806654
cristianb <cristianb@L-TI-10.tactic.local>
2016-10-12 14:15:27 -0500

    Ordenes de compra WMS JDA

--- src/main/java/com/tacticlogistics/application/services/clientes/dicermex/ResultadoPreAlertaType.java
+++
@@ -1,6 +0,0 @@
-package com.tacticlogistics.application.services.clientes.dicermex;
-
-public enum ResultadoPreAlertaType {
-	OK,ERROR
-
-}
--- src/main/java/com/tacticlogistics/application/tasks/schedules/oms/reglas/Regla.java
+++
@@ -1,7 +0,0 @@
-package com.tacticlogistics.application.tasks.schedules.oms.reglas;
-
-import com.tacticlogistics.application.dto.common.MensajesDto;
-
-public interface Regla<T> {
-	MensajesDto validar(T object);
-}
