d7f43102db48e9f28abdfe7febae86e96128be05
albam@tacticlogistics.com <albam@tacticlogistics.com>
2016-10-18 21:36:40 -0500

    Merge Compras

--- src/main/java/com/tacticlogistics/jda/wms/tasks/compras/EnviarOrdenCompra.java
+++ src/main/java/com/tacticlogistics/jda/wms/tasks/compras/EnviarOrdenCompra.java
@@ -8,7 +8,9 @@ import java.math.BigInteger;
 import java.sql.Date;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 import org.dom4j.Document;
@@ -24,15 +26,17 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import com.j256.ormlite.dao.Dao;
-import com.tacticlogistics.clientes.dicermex.compras.erp.prealertas.PreAlertasService;
 import com.tacticlogistics.clientes.dicermex.compras.wms.alertas.AlertasWmsService;
+import com.tacticlogistics.clientes.dicermex.compras.wms.alertas.ResultadoAlertaDto;
+import com.tacticlogistics.clientes.dicermex.compras.wms.alertas.ResultadoAlertaType;
 import com.tacticlogistics.domain.model.ordenes.LineaOrden;
 import com.tacticlogistics.domain.model.ordenes.Orden;
 import com.tacticlogistics.jda.wms.db.entities.Control;
-import com.tacticlogistics.jda.wms.db.entities.PurchaseOrder;
-import com.tacticlogistics.jda.wms.db.entities.PurchaseOrderLine;
+import com.tacticlogistics.jda.wms.db.entities.receiving.PurchaseOrder;
+import com.tacticlogistics.jda.wms.db.entities.receiving.PurchaseOrderLine;
 
 @Component
+@SuppressWarnings("unused")
 public class EnviarOrdenCompra {
 
 	private static final Logger log = LoggerFactory.getLogger(EnviarOrdenCompra.class);
@@ -40,6 +44,11 @@ public class EnviarOrdenCompra {
 	private static final String ACTION_CREATE = "A";
 	private static final String ACTION_UPDATE = "R";
 	private static final String ACTION_DELETE = "D";
+	private static final int STATUS_CREATED = 1;
+	private static final int STATUS_SUCCESS = 2;
+	private static final int STATUS_ERROR = 3;
+	private static final int STATUS_CONFIRMED = 4;
+	private static final int STATUS_SENT_FAIL = 5;
 
 	@Value("${wms.directorio.hostin}")
 	private String directorioHostIn;
@@ -58,14 +67,14 @@ public class EnviarOrdenCompra {
 	public void enviarOrdenesWMS() {
 		try {
 
-			List<OrdenCompra> ordenes = getOrdenesDeCompraHabilitadas();
+			List<PurchaseOrder> ordenes = getOrdenesDeCompraHabilitadas();
 
 			ordenes = consolidarLineasOrden(ordenes);
 
 			log.info("Ordenes Compra:" + ordenes.size());
 
-			for (OrdenCompra oc : ordenes) {
-				writeTransactionWMS(oc, ACTION_CREATE);
+			for (PurchaseOrder oc : ordenes) {
+				writePurchaseOrderInWMS(oc, ACTION_CREATE);
 			}
 
 			actualizarOrdenes(ordenes);
@@ -75,68 +84,126 @@ public class EnviarOrdenCompra {
 		}
 	}
 
-	private void writeTransactionWMS(OrdenCompra oc, String transactionType) throws SQLException {
-		
-		Control c = new Control();
-		c.setStatus(1);
-		c.setEvent_id(EVENT_PURCHASE_ORDER);
-		c.setWh_id(oc.getBodega());
-		c.setTrntyp(transactionType);
-		c.setCreate_date(new Date(new java.util.Date().getTime()));
-		controlDao.create(c);
+	@Scheduled(fixedRate = 1000 * 60 * 1)
+	public void confirmarOrdenesCreadas() {
+		try {
+			Map<String, Object> args = new HashMap<String, Object>();
+			args.put("status", STATUS_SUCCESS);
+			
+			List<Control> transactionsOK = controlDao.queryForFieldValuesArgs(args);
+			log.info("Ordenes OK:" + transactionsOK.size());
 
-		PurchaseOrder po = new PurchaseOrder();
-		po.setClient_id(oc.getClienteId());
-		po.setInvnum(oc.getNumeroOrden());
-		po.setSupnum(oc.getProveedor());
-		po.setWh_id(oc.getBodega());
-		po.setTransaction_id(c.getTransaction_id());
+			List<ResultadoAlertaDto> resultados = new ArrayList<>();
+			for (Control co : transactionsOK) {
+				args = new HashMap<String, Object>();
+				args.put("transaction_id", co.getTransaction_id());
+				PurchaseOrder oc = purchaseOrderDao.queryForFieldValues(args).get(0);
+				
+				resultados.add(ResultadoAlertaDto.builder().numeroOrdenWms(oc.getInvnum())
+						.resultado(ResultadoAlertaType.OK).build());
+			}
+			
+			ocService.confirmarResultadoDeAlertasAlWms(resultados);
+			
+			for (Control co : transactionsOK) {
+				co.setStatus(STATUS_CONFIRMED);
+				controlDao.update(co);
+			}
 
-		for (LineaOrdenCompra linea : oc.getLineas()) {
-			PurchaseOrderLine line = new PurchaseOrderLine();
-			line.setInvlin(linea.getNumeroLinea() + "");
-			line.setInvsln(linea.getNumeroSublinea());
-			line.setPrtnum(linea.getCodigoProducto());
-			line.setExpqty(linea.getCantidadEsperada());
-			line.setRcvsts(linea.getEstadoInventarioEsperado());
-
-			po.addLine(line);
-			purchaseOrderLineDao.create(line);
+		} catch (SQLException e) {
+			log.error(e.getMessage());
 		}
-		
-		purchaseOrderDao.create(po);
-
 	}
 
-	private List<OrdenCompra> getOrdenesDeCompraHabilitadas() {
+	@Scheduled(fixedRate = 1000 * 60 * 1)
+	public void confirmarOrdenesError() {
+		try {
+			Map<String, Object> args = new HashMap<String, Object>();
+			args.put("status", STATUS_ERROR);
+			
+			List<Control> transactionsOK = controlDao.queryForFieldValuesArgs(args);
+			log.info("Ordenes OK:" + transactionsOK.size());
+
+			List<ResultadoAlertaDto> resultados = new ArrayList<>();
+			for (Control co : transactionsOK) {
+				args = new HashMap<String, Object>();
+				args.put("transaction_id", co.getTransaction_id());
+				PurchaseOrder oc = purchaseOrderDao.queryForFieldValues(args).get(0);
+				
+				resultados.add(ResultadoAlertaDto.builder().numeroOrdenWms(oc.getInvnum())
+						.resultado(ResultadoAlertaType.ERROR).build());
+			}
+			
+			ocService.confirmarResultadoDeAlertasAlWms(resultados);
+			
+			for (Control co : transactionsOK) {
+				co.setStatus(STATUS_SENT_FAIL);
+				controlDao.update(co);
+			}
+
+		} catch (SQLException e) {
+			log.error(e.getMessage());
+		}
+	}
+	
+	/**
+	 * Create transaction in WMS intermediate tables
+	 * 1. create lines
+	 * 2. create header
+	 * 3. create control transaction
+	 */
+	private void writePurchaseOrderInWMS(PurchaseOrder oc, String transactionType) throws SQLException {
+		System.out.println("===== Beginning transaction WMS =====");
+		
+		//Create control transaction to get a Transaction_id
+		Control c = new Control();
+		c.setStatus(-1); //Status must be invalid at this point
+		c.setEvent_id(EVENT_PURCHASE_ORDER);
+		c.setWh_id(oc.getWh_id());
+		c.setTrntyp(transactionType);
+		c.setCreate_date(new Date(new java.util.Date().getTime()));
+		
+		controlDao.create(c);
+		System.out.println("Transaction Number: " + c.getTransaction_id());
+
+		for (PurchaseOrderLine line : oc.getLines()) {
+			purchaseOrderLineDao.create(line);
+		}
+
+		purchaseOrderDao.create(oc);
+		
+		//If everything else worked, update transaction status
+		c.setStatus(STATUS_CREATED);
+		controlDao.update(c);
+	}
+
+	private List<PurchaseOrder> getOrdenesDeCompraHabilitadas() {
 		List<Orden> ordenes = ocService.getOrdenesPendientesPorAlertarAlWms();
 
 		// @formatter:off
-		return ordenes
-				.stream()
-				.map(orden -> {
-			OrdenCompra oc = new OrdenCompra();
+		return ordenes.stream().map(orden -> {
+			PurchaseOrder oc = new PurchaseOrder();
 			String bodegaDestinoCodigo = orden.getLineas().stream().findFirst().get().getBodegaDestinoCodigo();
 
-			oc.setIdOrden(orden.getId());
-			oc.setNumeroOrden("TC-" + orden.getId() + '-' + orden.getNumeroOrden());
-			oc.setProveedor(orden.getCliente().getNumeroIdentificacion());
-			oc.setClienteId(orden.getCliente().getCodigoAlternoWms());
-			oc.setBodega(bodegaDestinoCodigo);
-			oc.setTipoOrden("REC");
-			oc.setEstadoOrden("OPEN");
+			oc.orderId = orden.getId();
+			oc.setInvnum("TC-" + orden.getId() + '-' + orden.getNumeroOrden());
+			oc.setSupnum(orden.getCliente().getNumeroIdentificacion());
+			oc.setClient_id(orden.getCliente().getCodigoAlternoWms());
+			oc.setWh_id(bodegaDestinoCodigo);
+			oc.setInvtyp("REC");
+			oc.setRimsts("OPEN");
 
 			for (LineaOrden e : orden.getLineas()) {
-				LineaOrdenCompra ocLinea = new LineaOrdenCompra();
+				PurchaseOrderLine ocLinea = new PurchaseOrderLine();
 
-				ocLinea.setNumeroLinea(e.getNumeroItem());
-				ocLinea.setCantidadEsperada(e.getCantidadPlanificada());
-				ocLinea.setCodigoProducto(e.getProductoCodigo());
-				ocLinea.setEstadoInventarioEsperado(e.getEstadoInventarioDestinoId());
-				ocLinea.setLote(e.getLote());
-				ocLinea.setEstampillado(e.getRequerimientoEstampillado());
+				ocLinea.setInvlin(e.getNumeroItem() + "");
+				ocLinea.setExpqty(e.getCantidadPlanificada());
+				ocLinea.setPrtnum(e.getProductoCodigo());
+				ocLinea.setRcvsts(e.getEstadoInventarioDestinoId());
+				ocLinea.setLotnum(e.getLote());
+				ocLinea.setInv_attr_str1(e.getRequerimientoEstampillado());
 
-				oc.addLinea(ocLinea);
+				oc.addLine(ocLinea);
 			}
 
 			return oc;
@@ -144,22 +211,22 @@ public class EnviarOrdenCompra {
 		// @formatter:on
 	}
 
-	private void actualizarOrdenes(List<OrdenCompra> ordenes) {
+	private void actualizarOrdenes(List<PurchaseOrder> ordenes) {
 		// @formatter:off
 		List<Integer> ordenesId = ordenes
 				.stream()
-				.map(orden -> orden.getIdOrden())
+				.map(orden -> orden.orderId)
 				.collect(Collectors.toList());
 		// @formatter:on
 
 		ocService.alertarOrdenesDeCompraAlWms(ordenesId);
 	}
 
-	private void writeFiles(OrdenCompra oc) throws IOException, UnsupportedEncodingException {
+	private void writeFiles(Orden oc) throws Exception {
 		OutputFormat format = OutputFormat.createPrettyPrint();
 
 		FileOutputStream fileOut = new FileOutputStream(
-				new File(directorioHostIn + "\\" + oc.getNumeroOrden() + ".xml"));
+				new File(directorioHostIn + "\\" + oc.getOrdenOriginalId() + ".xml"));
 
 		XMLWriter writer;
 
@@ -171,7 +238,7 @@ public class EnviarOrdenCompra {
 
 		writer.close();
 
-		File triggerFile = new File(directorioHostIn + "\\" + oc.getNumeroOrden() + ".trg");
+		File triggerFile = new File(directorioHostIn + "\\" + oc.getOrdenOriginalId() + ".trg");
 
 		triggerFile.createNewFile();
 	}
@@ -184,13 +251,13 @@ public class EnviarOrdenCompra {
 	 * @param ordenes
 	 * @return Lista consolidada
 	 */
-	public List<OrdenCompra> consolidarLineasOrden(List<OrdenCompra> ordenes) {
-		List<OrdenCompra> result = new ArrayList<>();
+	public List<PurchaseOrder> consolidarLineasOrden(List<PurchaseOrder> ordenes) {
+		List<PurchaseOrder> result = new ArrayList<>();
 
-		for (OrdenCompra oc : ordenes) {
+		for (PurchaseOrder oc : ordenes) {
 			if (result.contains(oc)) {
 				int pos = result.indexOf(oc);
-				result.get(pos).appendLineas(oc.getLineas());
+				result.get(pos).appendLines(oc.getLines());
 			} else {
 				result.add(oc);
 			}
@@ -199,34 +266,34 @@ public class EnviarOrdenCompra {
 		return result;
 	}
 
-	public Document formatDocument(OrdenCompra oc) {
+	public Document formatDocument(Orden orden) {
 		Document document = DocumentHelper.createDocument();
 		Element root = document.addElement("UC_RA_INB_IFD");
 		Element controlSegment = root.addElement("CTRL_SEG");
 
 		controlSegment.addElement("TRNNAM").addText("ORDCOMP_ESB");
 		controlSegment.addElement("TRNVER").addText("ESB2015.1");
-		controlSegment.addElement("WHSE_ID").addText(oc.getBodega());
+		controlSegment.addElement("WHSE_ID").addText(orden.getLineas().stream().findFirst().get().getBodegaDestinoCodigo());
 
 		Element header = controlSegment.addElement("HEADER_SEG");
 
 		header.addElement("SEGNAM").addText("CABECERA");
 		header.addElement("TRNTYP").addText("A");
-		header.addElement("INVNUM").addText(oc.getNumeroOrden());
-		header.addElement("SUPNUM").addText(oc.getProveedor());
-		header.addElement("INVTYP").addText(oc.getTipoOrden());
-		header.addElement("CLIENT_ID").addText(oc.getClienteId());
-		header.addElement("RIMSTS").addText(oc.getEstadoOrden());
+		header.addElement("INVNUM").addText("TC-" + orden.getId() + '-' + orden.getNumeroOrden());
+		header.addElement("SUPNUM").addText(orden.getCliente().getNumeroIdentificacion());
+		header.addElement("INVTYP").addText("RET");
+		header.addElement("CLIENT_ID").addText(orden.getCliente().getCodigoAlternoWms());
+		header.addElement("RIMSTS").addText("OPEN");
 
-		for (LineaOrdenCompra linea : oc.getLineas()) {
+		for (LineaOrden linea : orden.getLineas()) {
 			Element line = header.addElement("LINE_SEG");
 
 			line.addElement("SEGNAM").addText("LINEA");
-			line.addElement("INVLIN").addText(linea.getNumeroLinea() + "");
-			line.addElement("INVSLN").addText(linea.getNumeroSublinea() + "");
-			line.addElement("PRTNUM").addText(linea.getCodigoProducto());
-			line.addElement("EXPQTY").addText(linea.getCantidadEsperada() + "");
-			line.addElement("RCVSTS").addText(linea.getEstadoInventarioEsperado());
+			line.addElement("INVLIN").addText(linea.getNumeroItem() + "");
+			line.addElement("INVSLN").addText("0000");
+			line.addElement("PRTNUM").addText(linea.getProductoCodigo());
+			line.addElement("EXPQTY").addText(linea.getCantidadPlanificada() + "");
+			line.addElement("RCVSTS").addText(linea.getEstadoInventarioDestinoId());
 		}
 		return document;
 	}
